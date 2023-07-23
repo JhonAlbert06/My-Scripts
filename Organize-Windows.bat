b2ef70b56945d23a99779603d2573b0f8f34374c
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-07-23 11:24:01 -0400

    Update Organize-Windows.bat

--- Organize-Windows.bat
+++ Organize-Windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 1. El script comienza estableciendo la variable DOWNLOADS_DIRECTORY con la ruta del directorio de descargas del usuario (%USERPROFILE%\Downloads).
-set DOWNLOADS_DIRECTORY=%USERPROFILE%\Downloads
+set "DOWNLOADS_DIRECTORY=%USERPROFILE%\Downloads"
 
 REM 2. Luego, el script verifica si los directorios Images, Documents, Videos, Music y Others existen en el directorio de descargas. Si no existen, el script los crea utilizando el comando mkdir.
 if not exist "%DOWNLOADS_DIRECTORY%\Images\" mkdir "%DOWNLOADS_DIRECTORY%\Images"
@@ -11,19 +11,25 @@ if not exist "%DOWNLOADS_DIRECTORY%\Music\" mkdir "%DOWNLOADS_DIRECTORY%\Music"
 if not exist "%DOWNLOADS_DIRECTORY%\Others\" mkdir "%DOWNLOADS_DIRECTORY%\Others"
 if not exist "%DOWNLOADS_DIRECTORY%\Iso\" mkdir "%DOWNLOADS_DIRECTORY%\Iso"
 if not exist "%DOWNLOADS_DIRECTORY%\Compressed\" mkdir "%DOWNLOADS_DIRECTORY%\Compressed"
+if not exist "%DOWNLOADS_DIRECTORY%\Executables\" mkdir "%DOWNLOADS_DIRECTORY%\Executables"
+
+
 
 REM 3. Luego, el script utiliza un bucle for para iterar a través de todos los archivos en el directorio de descargas. Para cada archivo, el script verifica si es un archivo regular utilizando el comando if exist.
 
-REM 4. Si el archivo es un archivo regular, el script utiliza la expansión de parámetros ${file##*.} para extraer la extensión del archivo y la guarda en la variable extension.
+REM 4. Si el archivo es un archivo regular, el script utiliza los modificadores de variables del lote para extraer la extensión del archivo y guardarla en la variable "extension".
 
-REM 5. A continuación, el script utiliza una sentencia if-else para verificar la extensión del archivo y moverlo al directorio correspondiente según su extensión. Si la extensión no coincide con ninguno de los casos definidos, se mueve al directorio Others.
+REM 5. El script luego usa un caso de instrucción para verificar la extensión del archivo y moverlo al directorio correspondiente según su extensión. Si la extensión no coincide con ninguno de los casos definidos, se mueve al directorio Others.
 
 REM 6. El script utiliza el comando move para mover los archivos a sus respectivos directorios.
 
-for %%F in ("%DOWNLOADS_DIRECTORY%\*") do (
+for %%F in ("%DOWNLOADS_DIRECTORY%\*.*") do (
     if exist "%%F" (
-        set "file=%%~F"
-        set "extension=!file:~-3!"
+        set "file=%%~nxF"
+        set "extension=%%~xF"
+        setlocal enabledelayedexpansion
+        set "extension=!extension:~1!"
+        set "extension=!extension:~0,3!"
         if /i "!extension!"=="jpg" (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Images\%%~nxF"
         ) else if /i "!extension!"=="png" (
@@ -41,6 +47,8 @@ for %%F in ("%DOWNLOADS_DIRECTORY%\*") do (
         ) else if /i "!extension!"=="ppt" (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Documents\%%~nxF"
         ) else if /i "!extension!"=="pptx" (
+            move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Documents\%%~nxF"
+        ) else if /i "!extension!"=="xlsx" (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Documents\%%~nxF"
         ) else if /i "!extension!"=="mp4" (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Videos\%%~nxF"
@@ -70,9 +78,12 @@ for %%F in ("%DOWNLOADS_DIRECTORY%\*") do (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Compressed\%%~nxF"
         ) else if /i "!extension!"=="gz" (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Compressed\%%~nxF"
+        ) else if /i "!extension!"=="exe" (
+            move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Executables\%%~nxF"
         ) else (
             move /-Y "%%F" "%DOWNLOADS_DIRECTORY%\Others\%%~nxF"
         )
+        endlocal
     )
 )
 
